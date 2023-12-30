27bcc256c4e165f8f082c1ca8bc717378801baeb
Savus <justin.fuzzygreenstuff@gmail.com>
2023-12-30 00:31:24 -0700

    start over

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,5 @@
-import { useEffect, useState } from "react";
-import { ToDoListRequests } from "./api";
-import { NavbarHeader } from "./components/NavbarHeader";
-import "./css/base.css";
-import "./css/styles.css";
-import "./css/theme.css";
-import { ListItem } from "./types";
-import { ListComponent } from "./components/ToDoListApp.tsx/ListComponent";
-
-const useReact = false;
-
-const portfolioData = [
-  { title: "Home Development", dataFilter: "web" },
-  { title: "Home Development", dataFilter: "web" },
-  { title: "Home Development", dataFilter: "web" },
-  { title: "Home Development", dataFilter: "web" },
-  { title: "Home Development", dataFilter: "web" },
-];
-
 function App() {
-  const [listItems, setListItems] = useState<ListItem[]>([]);
-
-  const refetchData = () => {
-    return ToDoListRequests.getAllListItems().then(setListItems);
-  };
-
-  useEffect(() => {
-    refetchData();
-  }, []);
-
-  return (
-    <>
-      <main>
-        <NavbarHeader useReact={useReact} logo={"Logo"} />
-        <ListComponent useReact={useReact} itemsList={listItems} />
-      </main>
-    </>
-  );
+  return <>Hello World</>;
 }
 
 export default App;
