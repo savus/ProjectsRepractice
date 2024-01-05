f50182464df28cbe5f48de01116e3c665f0b4fca
Savus <justin.fuzzygreenstuff@gmail.com>
2024-01-05 06:07:09 -0700

    finish styling add-item form. next to hook up post fetch call for form text and submit inputs.

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -13,20 +13,26 @@ export class ToDoList extends Component {
             </div>
             <form action="#" id="add-item-form" className="flex-centered">
               <h3>New Item</h3>
-              <div className="primary-input">
+              <div className="input-primary">
                 <input
                   type="text"
                   id="new-item-input"
                   placeholder="type here..."
                 />
               </div>
+              <input
+                type="submit"
+                id="add-item-confirm"
+                value="Confirm"
+                className="btn btn-primary"
+              />
             </form>
           </header>
           <div id="to-do-body">
             <ul id="list-container" className="ul-defaults-none">
               <li className="list-item edit-mode flex-centered">
                 <div className="item-content">Shop</div>
-                <div className="item-input-container primary-input">
+                <div className="item-input-container input-primary">
                   <input
                     type="text"
                     className="item-input"
